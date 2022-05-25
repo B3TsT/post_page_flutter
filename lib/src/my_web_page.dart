37109f20ef2985c4a10a6694ba4ae929de229a1e
b3tst <b3tst@b3tst.bst>
2022-05-25 00:13:05 -0500

    maquetado basico

--- lib/src/my_web_page.dart
+++ lib/src/my_web_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:post_page/src/content/contact_content.dart';
+import 'package:post_page/src/content/features_content.dart';
+import 'package:post_page/src/content/home_content.dart';
+import 'package:post_page/src/content/screenshots_content.dart';
+import 'package:post_page/src/navigation_bar/nav_bar.dart';
 
 class MyWebPage extends StatelessWidget {
   const MyWebPage({Key? key}) : super(key: key);
@@ -11,14 +16,19 @@ class MyWebPage extends StatelessWidget {
           child: Column(
             children: [
               //Navigation Bar
+              NavBar(),
               Expanded(
                 child: SingleChildScrollView(
                   child: Column(
                     children: const [
                       // Home
+                      HomeContent(),
                       // Features
+                      FeaturesContent(),
                       // Screenshots
+                      ScreenshotsContent(),
                       // Contact
+                      ContactContent(),
                     ],
                   ),
                 ),
