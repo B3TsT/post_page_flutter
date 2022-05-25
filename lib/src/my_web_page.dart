cc1b7dba0a8c55ebe2e1cbe1bd2522e0bf0afa25
b3tst <b3tst@b3tst.bst>
2022-05-24 23:36:58 -0500

    Estructura de la pagina web

---
+++ lib/src/my_web_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyWebPage extends StatelessWidget {
+  const MyWebPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            children: const [],
+          ),
+        ),
+      ),
+    );
+  }
+}
